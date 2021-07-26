f9e67ce4f54bfa645d01b658597fb0239a2dc5a0
iotti0 <82509628+iotti0@users.noreply.github.com>
2021-07-26 22:28:45 +0700

    add essay body

--- src/pages/{Mdx.slug}.js
+++ src/pages/{Mdx.slug}.js
@@ -1,16 +1,30 @@
 import * as React from 'react'
 import {graphql} from 'gatsby'
+import {MDXRenderer} from 'gatsby-plugin-mdx'
 
 const EssayPostPage = ({data}) => {
-    return <h1>{data.mdx.frontmatter.title}</h1>
+
+    const essay = data.mdx
+
+    return (
+        <article>
+            <h1>{essay.frontmatter.title}</h1>
+            <p>Published in {essay.frontmatter.date}</p>
+            <p>{essay.frontmatter.read} read</p>
+            <MDXRenderer>{essay.body}</MDXRenderer>
+        </article>
+    )
 }
 
 export const query = graphql`
     query EssayPostById($id: String) {
         mdx(id: {eq: $id}) {
             frontmatter {
+                date(formatString: "MMMM D, YYYY")
                 title
+                read
             }
+        body
         }
     }`
 
