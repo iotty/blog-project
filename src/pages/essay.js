240df07fbedf0948bb72aeb9488bd2c3da9e924d
iotti0 <82509628+iotti0@users.noreply.github.com>
2021-07-26 09:36:38 +0700

    added necessary pages

---
+++ src/pages/essay.js
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+const EssayPage = () => {
+    return (
+        <div>This is Essay pages.</div>
+    )
+}
+
+export default EssayPage
---
+++ src/pages/illustration.js
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+const IllustrationPage = () => {
+    return (
+        <div>This is illustration pages.</div>
+    )
+}
+
+export default IllustrationPage
