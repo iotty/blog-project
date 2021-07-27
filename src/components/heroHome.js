1bef58b17d318a2fd00d1f133ea6e75f17e2c278
iotti0 <82509628+iotti0@users.noreply.github.com>
2021-07-27 21:03:15 +0700

    text desc on hero section

--- src/components/heroHome.js
+++ src/components/heroHome.js
@@ -6,7 +6,7 @@ const heroHome = ({name, description, slug}) => {
             <div className='p-4 md:px-16 max-w-screen-xl mx-auto'>
                 <h1 className='mt-15 text-gray-700 text-6xl font-black tracking-wide leading-tighter'>{name}</h1>
                 <h2 className='mt-12 font-medium font-serif text-2xl text-gray-600 tracking-normal leading-normal'>{description}</h2>
-                <p className='mt-8 text-xl text-gray-600 tracking-tighter leading-normal'>{slug}</p>
+                <p className='mt-8 text-xl text-gray-500 tracking-tighter leading-normal'>{slug}</p>
             </div>
         </section>
     )
