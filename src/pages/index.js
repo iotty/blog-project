53f6028b9b929cbc13ab0857a8814d28174cf7cd
iotti0 <82509628+iotti0@users.noreply.github.com>
2021-07-27 12:11:17 +0700

    add font inter

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,16 @@
 import * as React from "react"
 import Seo from '../components/seo'
+import ContainerPadding from '../components/containerPadding'
 
 
 const IndexPage = () => {
   return (
+    <div>
     <Seo title='Homepage' />
+ 
+      <h1>This will be heading.</h1>
+    
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans : ['InterVariable', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto'],
+      },
+    },
   },
   variants: {
     extend: {},
