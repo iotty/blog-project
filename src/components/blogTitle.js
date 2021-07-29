6ac911fe948a6199adc6fe8fecf9bc693e57b9f2
iotti0 <82509628+iotti0@users.noreply.github.com>
2021-07-29 16:16:25 +0700

    line height update

--- src/components/blogTitle.js
+++ src/components/blogTitle.js
@@ -3,7 +3,7 @@ import * as React from 'react'
 const blogTitle = ({postTitle, date}) => {
     return (
         <div className='my-12 border-b'>
-        <h1 className='text-4xl md:text-5xl md:leading-normal lg:text-6xl lg:leading-normal text-gray-700 font-bold text-center tracking-wide leading-snug'>{postTitle}</h1>
+        <h1 className='text-4xl md:text-5xl md:leading-snug lg:text-6xl lg:leading-snug text-gray-700 font-bold text-center tracking-wide leading-snug'>{postTitle}</h1>
         <p className='text-center my-4 text-gray-400'>Published on {date}</p>
         </div>
     )
