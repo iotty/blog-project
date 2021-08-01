d069cba33ef964009b2d1a1bf16145dc4f512f7b
iotti0 <82509628+iotti0@users.noreply.github.com>
2021-08-01 15:47:19 +0700

    edit color btn

--- src/pages/404.js
+++ src/pages/404.js
@@ -12,7 +12,7 @@ const NotFoundPage = () => {
           <h1 className='text-3xl md:text-4xl lg:text-5xl text-left font-extrabold text-gray-800'>Page not found</h1>
           <p className='text-lg mt-4'>Sorry 😔 —we couldn’t find what you were looking for.</p>
         <Link to='/' className='mt-8'>
-        <button type='button' className='py-2 px-4 border inline-block rounded outline-none font-medium text-red-700 hover:bg-red-600 hover:border-white hover:text-white transition duration-700 ease-in-out transform hover:scale-105'>
+        <button type='button' className='py-2 px-4 border inline-block rounded outline-none font-medium text-yellow-700 hover:bg-yellow-600 hover:border-white hover:text-white transition duration-700 ease-in-out transform hover:scale-105'>
           <div className='flex'>
             <p>Go back home</p>
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
